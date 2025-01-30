b09b6296268ec0ad5f8f2c1d6eccc9f558423635
kellywashere <31252870+kellywashere@users.noreply.github.com>
2025-01-30 19:23:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ let prevtime_ms = null; // previous timestamp gameloop
 let starttime_ms = null;
 let abstime_s = 0;     // abs time in s
 
+let gravity; // g
+
+const nrParticles = 80; // per explosion
+let particles = null;
+// particles: { posx, posy, vx, vy }
+
+let time_till_next_explosion;
 
 window.onload = function () {
 	// Init canvas size
@@ -12,6 +19,11 @@ window.onload = function () {
 	//const dpr = 1;
 	canvas.width = window.innerWidth * dpr;
 	canvas.height = window.innerHeight * dpr;
+
+	gravity = canvas.height * 0.2;
+	time_till_next_explosion = 11; // countdown
+
+	particles = initParticles(nrParticles, canvas.width/2, canvas.height/2, canvas.height * 0.15);
 	
 	requestAnimationFrame(gameloop);
 }
@@ -30,6 +42,12 @@ function gameloop(timestamp_ms) {
 		//updateFireworks(dt);
 		//renderFireworks();
 
+		update(dt);
+		render();
+
+
+
+		
 		color = randomColor();
 		let r = Math.round(color.r * 255);
 		let g = Math.round(color.g * 255);
@@ -52,6 +70,52 @@ function gameloop(timestamp_ms) {
 
 	requestAnimationFrame(gameloop);
 }
+
+function update(dt) {
+	ps = particles || [];
+	updateParticles(ps, dt);
+}
+
+function render() {
+	let w = canvas.width;
+	let h = canvas.height;
+	let radius = h / 300;
+
+	ctx.fillStyle = "red";
+	for (p of particles) {
+		ctx.beginPath();
+		ctx.arc(p.posx, p.posy, radius, 0, 2 * Math.PI);
+		ctx.fill();
+	}
+}
+
+function initParticles(n, x, y, scalev) {
+	// velocity is randomized point on sphere, see: https://mathworld.wolfram.com/SpherePointPicking.html
+	let ps = [];
+	while (n--) {
+		let x1, x2, d;
+		do {
+			x1 = 2 * Math.random() - 1;
+			x2 = 2 * Math.random() - 1;
+			d = 1 - x1*x1 - x2*x2;
+		} while (d <= 0);
+		let sqrtd = Math.sqrt(d);
+		ps.push({ posx: x, posy: y, vx: scalev * 2*x1*sqrtd, vy: scalev * 2*x2*sqrtd });
+	}
+	return ps;
+}
+
+function updateParticles(ps, dt) {
+	for (let ii = 0; ii < ps.length; ii++) {
+		let p = ps[ii];
+		p.posx += p.vx * dt;
+		p.posy += p.vy * dt;
+		p.vy += gravity * dt;
+	}
+}
+
+
+
 
 function randomColor() {
 	// hsl: h is random, s = 1, l=0.5;
